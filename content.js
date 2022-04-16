8b011b0976c7a8847097e4e82764d378b3f2f95d
Amir Sharapov <amir@amirfounder.com>
2022-04-16 18:10:27 -0500

    stuff

--- content.js
+++ content.js
@@ -1,20 +1,19 @@
 const INTERVAL_MS = 500
 
 let prev_mutations_observed = 0
-let mutations_observed = 0
+let new_mutations_observed = 0
 
 const observer = new MutationObserver((mutations, _) => {
-  mutations_observed += mutations.length
+  new_mutations_observed += mutations.length
 })
 
 const timeout_handler = () => {
-  const is_dom_mutating = mutations_observed - prev_mutations_observed !== 0
-  if (is_dom_mutating) {
+  if (new_mutations_observed - prev_mutations_observed > 0) {
     console.log('Dom still mutating. Waiting ...')
-    prev_mutations_observed = mutations_observed
+    prev_mutations_observed = new_mutations_observed
     setTimeout(timeout_handler, INTERVAL_MS)
   } else {
-    console.log('Dom finsihed mutating. Sending HTTP request to server ...')
+    console.log('Dom finished mutating. Sending document to server ...')
     observer.disconnect()
     fetch('http://localhost:8080', {
       method: 'POST',
